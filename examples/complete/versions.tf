d96559302b3952fb623fc285857cc27c754d7bb0
Nuru <Nuru@users.noreply.github.com>
2023-05-20 14:32:18 -0700

    Respond to automated feedback

--- examples/complete/versions.tf
+++ examples/complete/versions.tf
@@ -2,9 +2,17 @@ terraform {
   required_version = ">= 1.0"
 
   required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 4.9.0"
+    }
     helm = {
       source  = "hashicorp/helm"
       version = ">= 2.2"
+    }
+    kuberntes =  {
+      source  = "hashicorp/kubernetes"
+      version = ">= 2.7.1"
     }
   }
 }
