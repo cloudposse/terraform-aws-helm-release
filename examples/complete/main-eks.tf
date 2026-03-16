d3a145a36bff9495edb109285006d5647a8efeb4
RoseSecurity <michael@rosesecurity.dev>
2026-03-16 12:13:41 -0400

    feat(eks): upgrade eks-cluster module to v3.0.0
    - Update cloudposse/eks-cluster/aws module from v2.8.0 to v3.0.0
    - Replace deprecated resolve_conflicts with resolve_conflicts_on_create
      and resolve_conflicts_on_update for EKS addons
    - Add configuration_values field to addons variable type
    - Make addon fields optional with null defaults
    - Bump AWS provider minimum version to >= 5.0.0

--- examples/complete/main-eks.tf
+++ examples/complete/main-eks.tf
@@ -64,7 +64,7 @@ module "subnets" {
 
 module "eks_cluster" {
   source  = "cloudposse/eks-cluster/aws"
-  version = "2.8.0"
+  version = "3.0.0"
 
   region                       = var.region
   vpc_id                       = module.vpc.vpc_id
