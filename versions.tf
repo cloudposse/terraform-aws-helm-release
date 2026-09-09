0676483f35e520d4fa496fb65895191ced176acf
Michael Rosenfeld <72598486+RoseSecurity@users.noreply.github.com>
2026-09-09 14:24:44 -0400

    chore: update versions for compatibility and test fixtures

--- versions.tf
+++ versions.tf
@@ -5,11 +5,11 @@ terraform {
     # Update these to reflect the actual requirements of your module
     helm = {
       source  = "hashicorp/helm"
-      version = ">= 2.2"
+      version = ">= 2.2, < 3.0"
     }
     kubernetes = {
       source  = "hashicorp/kubernetes"
-      version = ">= 2.7.1"
+      version = ">= 2.7.1, < 3.0"
     }
   }
 }
